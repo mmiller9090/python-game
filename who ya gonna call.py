e560dfc3fe0abc4d5eb6620b98ec86ece3a1812d
mmiller9090 <mmiller20019@gmail.com>
2020-10-01 15:05:50 -0400

    Create who ya gonna call.py

---
+++ who ya gonna call.py
@@ -0,0 +1,36 @@
+
+print ("you walk into a dark room in a house which way do you turn left right")
+
+val = input("enter your choice: ")
+if val == "left":
+	print("you stub your toe")
+elif val == "right":
+	print("you trip")
+else:
+	print("you hit your head")
+
+print ("you are hurt and need help who ya gonna call")
+
+val = input("enter your choice: ")
+if val == "ghost busters" or "ghostbusters":
+	print(" you are a good person")
+else:
+	print("you should know the answer to this")
+
+if print == "you should know the answer to this":
+	print ("you loose")
+
+print("the Ghostbusters show up, but are slightly confused why you called")
+print("you tell them you have no idea but ask for their help searching the house")
+print("you all split up to search the house but bill murray sus as fuck")
+print("Bill Murray walks in the room you are in by yourself, the tension could cut steel.")
+print("what do you do?")
+print("1.run away")
+print("2.fight bill murray")
+val =input("enter 1 or 2   ")
+if val == (1):
+	print("you turn to run but hit your head on a low hanging light, Bill Murray stabs you in the face and begins to eat your flesh while you die")
+	print("you have lost against the imposter")
+else:
+	print("you fight off Bill Murray and inform the others, Bill Murray is found to be the imposter.")
+	print("Congrats you win and have found the imposter")
